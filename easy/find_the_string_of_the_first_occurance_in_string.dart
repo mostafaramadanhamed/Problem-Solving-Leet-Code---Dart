b8cb77872c0edf63eaceaa0bff64eae9c3449e19
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-05-10 11:11:55 +0300

    edit

--- easy/find_the_string_of_the_first_occurance_in_string.dart
+++ easy/find_the_string_of_the_first_occurance_in_string.dart
@@ -1,5 +1,6 @@
 void main(){
-
+  Soultion solution = Soultion();
+  print(solution.strStr("hello", "ll"));
 }
 
 //my solution
@@ -9,10 +10,6 @@ class  Soultion {
   // best solution
   int strStr(String haystack, String needle) {
   int index = haystack.indexOf(needle);
-  if (index == -1) {
-    return -1;
-  } else {
     return index;
   }
-}
 }
