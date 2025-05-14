e365c81a1ed700bc22ea8404b8c203db54186f2e
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-05-14 13:07:10 +0300

    - my solution

--- easy/sqrt.dart
+++ easy/sqrt.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 void main(){
   Solution solution = Solution();
   print(solution.mySqrt(4)); // Output: 2
@@ -7,19 +9,23 @@ void main(){
 }
 class Solution{
 
-  int mySqrt(int x) {
-    if (x < 2) return x;
-    int left = 2, right = x ~/ 2;
-    while (left <= right) {
-      int mid = left + (right - left) ~/ 2;
-      int square = mid * mid;
-      if (square == x) return mid;
-      if (square < x) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    return right;
-  }
+// my solution
+// https://leetcode.com/problems/sqrtx/description/
+  int mySqrt(int x) => sqrt(x).toInt();
+// best solution
+  // int mySqrt(int x) {
+  //   if (x < 2) return x;
+  //   int left = 2, right = x ~/ 2;
+  //   while (left <= right) {
+  //     int mid = left + (right - left) ~/ 2;
+  //     int square = mid * mid;
+  //     if (square == x) return mid;
+  //     if (square < x) {
+  //       left = mid + 1;
+  //     } else {
+  //       right = mid - 1;
+  //     }
+  //   }
+  //   return right;
+  // }
 }
